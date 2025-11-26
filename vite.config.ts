2d78f7d574d2f549c87b61190402b8389eac3605
farhodsaraev <farhodsaraev24@gmail.com>
2025-11-27 03:53:49 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, (process as any).cwd(), '');
   return {
     plugins: [react()],
-    base: "/repository-name/", 
     define: {
       'process.env.API_KEY': JSON.stringify(env.API_KEY),
     },
